adfb0d021f44461041f89c419b6d2892e1293c42
Michael Wolfe <353284+mwolfetech@users.noreply.github.com>
2025-01-24 23:11:39 -0500

    Cleanup; Array method returns double array

--- noc/src/vector/vector.clj
+++ noc/src/vector/vector.clj
@@ -3,25 +3,15 @@
   (:require [clojure.core.matrix :as m]))
 
 
-(defprotocol IVectorsa
+(defprotocol IVector
   (add ^Vector [^Vector this ^Vector that])
-  ;; (set [x])
-  ;; (set [x y])
-  ;; (set [x y z])
-  ;; (rem [x])
-  ;; (rem [x y])
-  ;; (rem [x y z]) 
-  ;; (sub [x])
-  ;; (sub [x y])
-  ;; (sub [x y z])
-  ;; (mult [x])
-  ;; (mult [x y])
-  ;; (mult [x y z])
-  ;; (div [x])
-  ;; (div [x y])
-  ;; (div [x y z])
+  ;;(set [this x] [this x y] [this x y z])
+  ;; (rem [x] [x y] [x y z]) 
+  ;; (sub [x] [x y] [x y z])
+  ;; (mult [x] [x y] [x y z])
+  ;; (div [x] [x y] [x y z])
   ;; (mag [this]) 
-  ;; (mag-sq [this])
+  ;; (mag-sq [this]) 
   ;; (dot [this v])
   ;; (cross [this v])
   ;; (dist [this v])
@@ -29,16 +19,14 @@
   ;; (limit [this, limit])
   ;; (set-mag [this, mag])
   ;; (heading [this])
-  ;; (set-heading [this, angle])
+  ;; (set-heading [this angle])
   ;; (rotate [this, amt])
   ;; (angle-between [this v])
   ;; (lerp [this v amt])
   ;; (slerp [this v amt])
   ;; (refect [this amt])
-  (get-array [this])
-  ;; (equals [this x])
-  ;; (equals [this x y])
-  ;; (equals [this x y z])
+  (array [this])
+  ;; (equals [this x] [this x y] [this x y z])
   ;; (from-angle [a])
   ;; (from-angles [a b])
   ;; (random-2d)
@@ -46,13 +34,11 @@
   ;; (clamp-to-zero [this])
 )
  
-(defn vector-add [a b]
-  (m/add a b)) 
-
 (deftype Vector [^:volatile-mutable va]
-  IVectorsa
-  (add [this that] (set! va (vector-add va (.-va ^Vector that))) this)
-  (get-array [this] (m/clone va)))
+  IVector
+  (add [this that] (m/add! va (.-va ^Vector that)) this)
+  (array [this] (double-array (m/clone va))))
 
 (defn ^Vector make-vector [x y z]
+  (m/set-current-implementation :vectorz)
   (Vector. (m/array [x y z]))) 
