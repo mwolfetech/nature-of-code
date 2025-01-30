1560761535c8eca4a6af55d3232bc6073a46c020
Michael Wolfe <353284+mwolfetech@users.noreply.github.com>
2025-01-29 22:23:01 -0500

    Add arity-2 implementation of rem

--- noc/src/vector/vector.clj
+++ noc/src/vector/vector.clj
@@ -6,8 +6,8 @@
 (defprotocol IVector
   (add ^Vector [^Vector this ^Vector that])
   (set ^Vector [^Vector this x] [^Vector this x y] [^Vector this x y z])
-  (rem ^Vector [^Vector this amt])
-  ;; (rem [x y] [x y z]) 
+  (rem ^Vector [^Vector this amt] [^Vector this x y])
+  ;; (rem [x y z]) 
   ;; (sub [x] [x y] [x y z])
   ;; (mult [x] [x y] [x y z])
   ;; (div [x] [x y] [x y z])
@@ -51,7 +51,10 @@
                         (m/mset 1 y)
                         (m/mset 0 x)
                         (Vector.)))
-  (rem [this amt] (m/emap! #(clojure.core/rem (int %) amt) va) this))
+  (rem [this amt] (m/emap! #(clojure.core/rem (int %) amt) va) this)
+  (rem [this x y] (m/assign! (m/subvector va 0 2) [(clojure.core/rem (m/mget va 0) x) 
+                                                   (clojure.core/rem (m/mget va 1) y)]) this)
+)
 
 (defn ^Vector make-vector [x y z]
   (m/set-current-implementation :vectorz)
